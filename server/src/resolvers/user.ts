aea092e5b7e02932c10457a5b57e7c9d588d64aa
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-05 18:26:43 +0200

    restructure the folders

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import argon2 from 'argon2'
-import { Resolver, Ctx, Arg, Mutation, InputType, Field, ObjectType } from 'type-graphql'
+import { Resolver, Ctx, Arg, Mutation, InputType, Field, ObjectType, Query } from 'type-graphql'
 import { User } from '../entities/User'
 import { MyContext } from '../types'
 
@@ -29,10 +29,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+    @Query(() => User, {nullable:true})
+    me(
+        @Ctx() {req, em}: MyContext
+    ) {
+        if (!req.session.userId) {
+            return null
+        }
+        const user = em.findOne(User, {id: req.session.userId})
+        return user
+    }
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         if (options.username.length <= 2) {
             return {
@@ -67,6 +77,10 @@ export class UserResolver {
             }
             console.log(`error:`, e.message)
         }
+        //store user id session
+        //this will set a cookie
+        req.session!.userId = user.id
+        
         return {
             user
         }
