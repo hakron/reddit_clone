832da53ccb6829317b6bf7e6469311b41d6ca91f
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-07 17:02:54 +0200

    added ssr to graphql

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { Provider } from "urql";
-import { client } from "../utils/graphqlClient";
-
-
 import theme from "../theme";
-
-
-
 
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
         <ColorModeProvider
           options={{
@@ -21,7 +13,6 @@ function MyApp({ Component, pageProps }: any) {
           <Component {...pageProps} />
         </ColorModeProvider>
       </ChakraProvider>
-    </Provider>
   );
 }
 
