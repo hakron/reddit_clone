7fbd65325d62cb47a91dfb07d30aa64dec895435
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-07 12:37:33 +0200

    added graphlcache to update the cache

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     );
   }
   return (
-    <Flex bg="tomato" p={4} ml={"auto"}>
+    <Flex bg="tan" p={4} ml={"auto"}>
       <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,14 +1,13 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { Provider } from "urql";
+import { client } from "../utils/graphqlClient";
+
 
 import theme from "../theme";
 
-const client = createClient({
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: 'include'
-  }
-});
+
+
+
 
 function MyApp({ Component, pageProps }: any) {
   return (
