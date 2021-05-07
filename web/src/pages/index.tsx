f77176ab2b9d16512f78ce7fe5c478ae738c0b6a
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-07 12:17:44 +0200

    created a Navbar to use it in the main page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from "../components/Navbar";
 
 const Index = () => (
-  <div>
-    Hello World
-  </div>
-)
+  <>
+    <Navbar />
+    <div>Hello World</div>
+  </>
+);
 
-export default Index
+export default Index;
