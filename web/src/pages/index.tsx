190ff398b588906f445a1183f914fbba05936e0c
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-07 17:06:24 +0200

    created the posts query

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { withUrqlClient } from "next-urql";
 import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlCLient";
 
-const Index = () => (
+const Index = () => {
+  const [{data}] = usePostsQuery()
+  return (
   <>
     <Navbar />
     <div>Hello World</div>
+    {!data ? null : data.posts.map((p => <div key={p.id}>{p.title}</div>))}
   </>
-);
+  )
+}
 
 export default withUrqlClient(createUrqlClient)(Index)
