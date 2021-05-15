dade600b6e06b1d1d5ac74f444508ff51ba70a19
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-15 15:57:04 +0200

    create inner joint for the post to add the creator

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -18,13 +18,14 @@ export class Post extends BaseEntity {
   text!: string;
 
   @Field()
-  @Column({type: 'int', default: 0})
+  @Column({ type: 'int', default: 0 })
   points!: number;
 
   @Field()
   @Column()
   title!: string;
 
+  @Field()
   @ManyToOne(() => User, user => user.posts)
   creator: User;
 
