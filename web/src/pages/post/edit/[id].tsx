ca38a1d135f33973bab7134d05611229ed2e7268
DESKTOP-N9CL1HF\enriq <hakronardo@gmail.com>
2021-05-19 16:51:12 +0200

    user is able to edit his post

--- web/src/pages/post/edit/[id].tsx
+++ web/src/pages/post/edit/[id].tsx
@@ -1,10 +1,81 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { InputField } from "../../../components/InputField";
+import { Layout } from "../../../components/Layout";
+import {
+  usePostQuery,
+  useUpdatePostMutation,
+} from "../../../generated/graphql";
 import { createUrqlClient } from "../../../utils/createUrqlClient";
+import useGetIntId from "../../../utils/useGetIntId";
 
 interface EditPostProps {}
 
 const EditPost: React.FC<EditPostProps> = ({}) => {
-  return <div>hello</div>;
+  const router = useRouter();
+  const intId = useGetIntId();
+  const [{ data, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+  const [, updatePost] = useUpdatePostMutation();
+  if (fetching) {
+    return (
+      <Layout>
+        <div>loading....</div>
+      </Layout>
+    );
+  }
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find a post</Box>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: data.post.title, text: data.post.text }}
+        onSubmit={async (values) => {
+          //   const { error } = await updatePost({ input: values });
+          //   if (!error) {
+          //     router.push("/");
+          //   }
+          updatePost({ id: intId, ...values });
+          router.push("/");
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" label="Title" placeholder="Title" />
+            <Box mt={4}>
+              <InputField
+                textArea
+                name="text"
+                label="Body"
+                placeholder="Text...."
+              />
+            </Box>
+            <Button
+              type="submit"
+              colorScheme="teal"
+              mt={4}
+              isLoading={isSubmitting}
+            >
+              Update post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient)(EditPost);
